2439df0a14dcf8b3f6240d687d83dbf472b4e46d
StelPan <20step01@gmail.com>
2021-09-14 19:41:01 +0700

    Create dist app

---
+++ dist/precache-manifest.67bfe1d656dee7030642a48f69ff5879.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0019d793a03c2986c3b1",
+    "url": "/css/app.0e433876.css"
+  },
+  {
+    "revision": "25dd9daef43a1b25810e",
+    "url": "/css/chunk-vendors.c7db8543.css"
+  },
+  {
+    "revision": "5c7f37c44d2423bac4ae8224a2f7e316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0019d793a03c2986c3b1",
+    "url": "/js/app.d69dfece.js"
+  },
+  {
+    "revision": "25dd9daef43a1b25810e",
+    "url": "/js/chunk-vendors.1222a346.js"
+  },
+  {
+    "revision": "732662f22f23e3a6a8ddeacee4a93604",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
